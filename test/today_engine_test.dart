b2fa8178c860ddac55e255dc5344623d5d3e54a5
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-16 06:42:30 +0600

    fix: import json support for today engine tests

--- test/today_engine_test.dart
+++ test/today_engine_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:study_os/models/study_models.dart';
