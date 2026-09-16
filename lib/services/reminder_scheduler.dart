a0ff22f0cfb8d2c290bdf8d412f378e5c0fe45cf
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-16 15:21:18 +0600

    refactor: extract reminder scheduler contract

---
+++ lib/services/reminder_scheduler.dart
@@ -0,0 +1,15 @@
+abstract interface class ReminderScheduler {
+  Future<void> scheduleDailyReminder({
+    required int id,
+    required String title,
+    required String body,
+    required int hour,
+    required int minute,
+  });
+
+  Future<void> showNow({required int id, required String title, required String body});
+
+  Future<void> cancel(int id);
+
+  Future<bool?> requestPermissions();
+}
