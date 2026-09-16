88f439a6f9243ef8c1808ce5801f6dc3cf17f7ae
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-16 11:00:08 +0600

    fix: import Flutter material types in planner test

--- test/regular_study_planner_test.dart
+++ test/regular_study_planner_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:study_os/models/study_models.dart';
