2243b11ca61d2aa01f72b025eb78ba0f20d112db
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-18 10:34:23 +0600

    defer non-critical startup work on release builds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'models/study_models.dart';
@@ -73,7 +74,13 @@ class _StudyOSState extends State<StudyOS> with WidgetsBindingObserver {
     WidgetsBinding.instance.addObserver(this);
     reminderCoordinator = ReminderCoordinator(store: widget.store, settingsStore: ReminderSettingsStore(widget.prefs), scheduler: NotificationService());
     _tabs[0] = _buildHomeTab();
-    WidgetsBinding.instance.addPostFrameCallback((_) { unawaited(_syncRemindersSafely()); });
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (kReleaseMode) {
+        unawaited(Future<void>.delayed(const Duration(milliseconds: 800), _syncRemindersSafely));
+      } else {
+        unawaited(_syncRemindersSafely());
+      }
+    });
   }
 
   Future<void> _syncRemindersSafely() async {
--- lib/widgets/update_gate.dart
+++ lib/widgets/update_gate.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
@@ -23,6 +24,9 @@ class _UpdateGateState extends State<UpdateGate> {
   Future<UpdateInfo?> _runCheck() async {
     try {
       if (widget.checkForUpdate != null) return await widget.checkForUpdate!();
+      if (kReleaseMode) {
+        await Future<void>.delayed(const Duration(milliseconds: 1000));
+      }
       final packageInfo = await PackageInfo.fromPlatform();
       return await UpdateService(currentVersion: packageInfo.version, prefs: widget.store.prefs).checkForUpdate();
     } catch (_) {
