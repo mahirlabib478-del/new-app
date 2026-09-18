4e8161141cba015ce07aa0b63417df1c41707d12
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-18 19:51:57 +0600

    fix release static analysis for notification build

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'models/study_models.dart';
