81efa4a7663f29f6f3e5bb765e453579d92af0d8
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-18 11:35:25 +0600

    tighten study hub hero vertical padding

--- lib/main.dart
+++ lib/main.dart
@@ -500,7 +500,7 @@ class _StudyHero extends StatelessWidget {
         borderRadius: BorderRadius.circular(22),
         onTap: onTap,
         child: Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.fromLTRB(16, 14, 16, 14),
           child: Row(
             children: [
               Container(
