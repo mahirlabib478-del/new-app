16abdd7719f3e38a1b224d03d99cdc43f4ed3b34
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-20 03:21:34 +0600

    chore: remove unused navigation import

--- app/src/main/kotlin/com/aistudio/studyos/MainActivity.kt
+++ app/src/main/kotlin/com/aistudio/studyos/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
