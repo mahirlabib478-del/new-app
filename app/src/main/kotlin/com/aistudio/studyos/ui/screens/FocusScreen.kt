536c591d4413473b5c635a65cec397e37ea452e9
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-20 21:50:36 +0600

    Clean ambient focus screen imports

--- app/src/main/kotlin/com/aistudio/studyos/ui/screens/FocusScreen.kt
+++ app/src/main/kotlin/com/aistudio/studyos/ui/screens/FocusScreen.kt
@@ -41,7 +41,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
