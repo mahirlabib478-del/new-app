a1d4fbf7549a5d31ec57e678856c1647802085e6
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-20 22:41:13 +0600

    Fix history button build import

--- app/src/main/kotlin/com/aistudio/studyos/ui/screens/ProgressScreen.kt
+++ app/src/main/kotlin/com/aistudio/studyos/ui/screens/ProgressScreen.kt
@@ -39,6 +39,7 @@ import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
