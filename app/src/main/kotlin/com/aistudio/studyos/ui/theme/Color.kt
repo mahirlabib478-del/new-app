c77d4e89667cedceed6c6036bdb78fb8dd8ca4da
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-26 19:27:48 +0600

    Remove unused colors for deleted themes

--- app/src/main/kotlin/com/aistudio/studyos/ui/theme/Color.kt
+++ app/src/main/kotlin/com/aistudio/studyos/ui/theme/Color.kt
@@ -7,37 +7,15 @@ val IndigoPrimary = Color(0xFF6366F1)
 val IndigoLight = Color(0xFF818CF8)
 val IndigoDark = Color(0xFF4F46E5)
 
-// Midnight Theme
-val MidnightBg = Color(0xFF0C0E17)
-val MidnightSurface = Color(0xFF131726)
-val MidnightCard = Color(0xFF1B2036)
-
 // Pitch Black Theme
 val PitchBlackBg = Color(0xFF000000)
 val PitchBlackSurface = Color(0xFF101010)
 val CyanAccent = Color(0xFF00E5FF)
 
-// New Dark Themes
-val MidnightIndigoBg = Color(0xFF0B1020)
-val MidnightIndigoSurface = Color(0xFF151B2F)
-val MidnightIndigoPrimary = Color(0xFF6366F1)
-val MidnightIndigoAccent = Color(0xFF818CF8)
-
 val DeepTealBg = Color(0xFF071414)
 val DeepTealSurface = Color(0xFF102222)
 val DeepTealPrimary = Color(0xFF14B8A6)
 val DeepTealAccent = Color(0xFF2DD4BF)
-
-// New Light Themes
-val CleanIndigoBg = Color(0xFFF8FAFC)
-val CleanIndigoSurface = Color.White
-val CleanIndigoPrimary = Color(0xFF4F46E5)
-val CleanIndigoAccent = Color(0xFF6366F1)
-
-val SoftMintBg = Color(0xFFF5FBFA)
-val SoftMintSurface = Color.White
-val SoftMintPrimary = Color(0xFF0F766E)
-val SoftMintAccent = Color(0xFF14B8A6)
 
 // Ocean Theme
 val OceanBg = Color(0xFF08121E)
