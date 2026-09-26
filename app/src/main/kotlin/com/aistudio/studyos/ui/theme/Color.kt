e77a4eaeeb78fdab21da099109c16db69eede978
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-26 19:51:13 +0600

    Remove Dracula Pro and Deep Teal themes

--- app/src/main/kotlin/com/aistudio/studyos/ui/theme/Color.kt
+++ app/src/main/kotlin/com/aistudio/studyos/ui/theme/Color.kt
@@ -12,11 +12,6 @@ val PitchBlackBg = Color(0xFF000000)
 val PitchBlackSurface = Color(0xFF101010)
 val CyanAccent = Color(0xFF00E5FF)
 
-val DeepTealBg = Color(0xFF071414)
-val DeepTealSurface = Color(0xFF102222)
-val DeepTealPrimary = Color(0xFF14B8A6)
-val DeepTealAccent = Color(0xFF2DD4BF)
-
 // Ocean Theme
 val OceanBg = Color(0xFF08121E)
 val OceanSurface = Color(0xFF0F1E30)
@@ -37,8 +32,3 @@ val CyberpunkSurface = Color(0xFF1B1233)
 val CyberpunkPink = Color(0xFFFF2A85)
 val CyberpunkCyan = Color(0xFF00F0FF)
 
-// Dracula Pro Theme
-val DraculaBg = Color(0xFF21222C)
-val DraculaSurface = Color(0xFF343746)
-val DraculaPurple = Color(0xFFBD93F9)
-val DraculaGreen = Color(0xFF50FA7B)
